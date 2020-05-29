759d08760fa3fdabd0d8fa5c37738437b0111fa5
jearmstrong21 <jearmstrong21@gmail.com>
2020-05-28 20:18:13 -0700

    fixhot
    Signed-off-by: jearmstrong21 <jearmstrong21@gmail.com>

--- src/main/java/p0nki/p0nkisassistant/commands/MembersCommands.java
+++ src/main/java/p0nki/p0nkisassistant/commands/MembersCommands.java
@@ -2,10 +2,7 @@ package p0nki.p0nkisassistant.commands;
 
 import net.dv8tion.jda.api.OnlineStatus;
 import p0nki.commandparser.command.CommandDispatcher;
-import p0nki.p0nkisassistant.utils.CommandResult;
-import p0nki.p0nkisassistant.utils.CommandSource;
-import p0nki.p0nkisassistant.utils.Nodes;
-import p0nki.p0nkisassistant.utils.Requirements;
+import p0nki.p0nkisassistant.utils.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,14 +14,14 @@ public class MembersCommands {
         dispatcher.register(Nodes.literal("allmembers")
                 .requires(Requirements.IN_GUILD)
                 .category("members")
-                .documentation("Counts top roles all members")
+                .documentation("Counts top roles of all members")
                 .executes(context -> {
                     Map<String, Integer> roles = new HashMap<>();
                     context.source().guild().getMembers().forEach(member -> {
                         String name = member.getRoles().size() > 0 ? member.getRoles().get(0).getAsMention() : "no top role";
                         roles.put(name, 1 + roles.getOrDefault(name, 0));
                     });
-                    context.source().channel().sendMessage(roles.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).collect(Collectors.joining("\n"))).queue();
+                    context.source().channel().sendMessage(new CustomEmbedBuilder().success().source(context.source()).title("Top roles of all members").description(roles.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).collect(Collectors.joining("\n"))).build()).queue();
                     return CommandResult.SUCCESS;
                 })
         );
@@ -38,7 +35,7 @@ public class MembersCommands {
                         String name = member.getOnlineStatus() == OnlineStatus.ONLINE ? (member.getRoles().size() > 0 ? member.getRoles().get(0).getAsMention() : "no top role") : "offline";
                         roles.put(name, 1 + roles.getOrDefault(name, 0));
                     });
-                    context.source().channel().sendMessage(roles.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).collect(Collectors.joining("\n"))).queue();
+                    context.source().channel().sendMessage(new CustomEmbedBuilder().success().source(context.source()).title("Top roles of online members").description(roles.entrySet().stream().map(entry -> entry.getKey() + ": " + entry.getValue()).collect(Collectors.joining("\n"))).build()).queue();
                     return CommandResult.SUCCESS;
                 })
         );
